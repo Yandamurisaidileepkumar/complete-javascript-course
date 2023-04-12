9e8c08b94cc30f717def742a49aa08e805f9cea0
Yandamuriisaidileepkumar <yandamuridileepkumar16@gmail.com>
2023-04-12 18:47:03 +0530

    okie

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
-
+"use strict";
+console.log(2023 - 1998);
+console.log(2023 - 1998);
+console.log(2023 - 1998);
+console.log(2023 - 1998);
+console.log(2023 - 1998);
+console.log(2023 - 1998);
+console.log(2023 - 1998);
+console.log(2023 - 1998);
+console.log(2023 - 1998);
+console.log(2023 - 1998);
